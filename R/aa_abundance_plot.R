bf3aea57d6d37b43e92d132a844ff37af2c6a997
madscort <madsn@msn.com>
2022-03-21 10:59:07 +0000

    Add amino acid abundance function and its dependencies

---
+++ R/aa_abundance_plot.R
@@ -0,0 +1,29 @@
+#' Plot abundance of unique amino acids in sequence
+#'
+#' @param aa_seq string of amino acids
+#'
+#' @importFrom magrittr %>%
+aa_abundance_plot <- function(aa_seq){
+
+  # Extract unique amino acids from sequence
+  aa_unique <- aa_seq %>%
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
+    as.character() %>%
+    unique()
+
+  # Count instances of each amino acid and add to matrix
+  counts <- sapply(aa_unique, function(aa_plot) stringr::str_count(string = aa_seq, pattern =  aa_plot)) %>%
+    as.data.frame()
+
+  # Create column and row names for matrix
+  colnames(counts) <- c("Counts")
+  counts[["Amino_acid"]] <- rownames(counts)
+
+  # Plot the amino acid abundances
+  aa_plot <- counts %>%
+    ggplot2::ggplot(ggplot2::aes(x = Amino_acid, y = Counts, fill = Amino_acid)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw()
+
+  return(aa_plot)
+}
