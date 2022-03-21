458c70768ea196ca7346635fc1a75e641bc4d903
bioniel <dounniel@icloud.com>
2022-03-21 09:00:23 +0000

    Data incorporation (task 2)

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' RNA codons
+#' 
+#' Translation codons from RNA to AA
+#' 
+#' @format
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"RNA"
+
+
